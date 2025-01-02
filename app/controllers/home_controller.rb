040f052a70f3a2502ec88622ff2dad82906a8745
Hamamoto Manabu <hamamotomanabu@Mac.AirPort>
2025-01-02 15:52:26 +0900

    deviseの導入

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
+  before_action :redirect_unlogged_users, only: [:index]
   def index
+  end
+
+  private
+
+  def redirect_unlogged_users
+    unless user_signed_in?
+      redirect_to new_user_registration_path
+    end
   end
 end
