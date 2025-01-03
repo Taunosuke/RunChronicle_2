06606da0922f170fdf905119619a39d55fca9f94
Hamamoto Manabu <hamamotomanabu@Mac.AirPort>
2025-01-03 22:28:17 +0900

    大会予定の投稿機能(コントローラ、ビュー)

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -6,7 +6,7 @@ class RacesController < ApplicationController
   def create
     @race = Race.new(race_params)
     if @race.save
-      redirect_to @race, notice: 'Race was successfully created.'
+      redirect_to @race, notice: "Race was successfully created."
     else
       render :new
     end
