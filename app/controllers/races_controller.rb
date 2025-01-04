c2d481d44d0fafb3eb67bfdceba76cf9722006e0
Hamamoto Manabu <hamamotomanabu@Mac.AirPort>
2025-01-04 15:03:09 +0900

    大会予定ページの追加

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,15 +1,23 @@
 class RacesController < ApplicationController
+  before_action :authenticate_user!
+
   def new
     @race = Race.new
   end
 
   def create
     @race = Race.new(race_params)
+    @race.user_id = current_user.id
     if @race.save
       redirect_to @race, notice: "Race was successfully created."
     else
       render :new
     end
+  end
+
+  def index
+    @races = current_user.races
+    puts @races.inspect
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_02_084324) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_04_035546) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_02_084324) do
     t.date "payment_due_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
