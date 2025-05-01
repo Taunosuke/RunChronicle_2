07197cee8fec461ca22d8de957485d6251c42914
Hamamoto Manabu <hamamotomanabu@MacBook-Air-2.local>
2025-05-01 22:02:07 +0900

    試合日程の更新機能の実装

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,5 +1,6 @@
 class RacesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_race, only: %i[edit update]
 
   def new
     @form = RaceForm.new
@@ -27,12 +28,29 @@ class RacesController < ApplicationController
   def destroy
     race = Race.find(params[:id])
     race.destroy
-    redirect_to races_path, notice: "大会予定を削除しました・"
+    redirect_to races_path, notice: "大会予定を削除しました。"
+  end
+
+  def edit
+    @race = RaceForm.new(race: @race)
+  end
+
+  def update
+    @race = RaceForm.new(race_params, race: @race)
+    if @race.save
+      redirect_to @race, notice:'大会予定を更新しました。'
+    else
+      render :edit
+    end
   end
 
   private
 
   def race_params
     params.require(:race).permit(:name, :date, :event, :distance, :payment_due_date).merge(user_id: current_user.id)
+  end
+
+  def set_race
+    @race = Race.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  post "/races/:id/edit", to: "races#update"
 end
