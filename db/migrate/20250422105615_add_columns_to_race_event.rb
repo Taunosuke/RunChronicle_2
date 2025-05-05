432a1f028893affcfcf9d5c9d952c5c664905780
Hamamoto Manabu <hamamotomanabu@MacBook-Air-2.local>
2025-05-05 16:04:39 +0900

    Lint修正

--- db/migrate/20250422105615_add_columns_to_race_event.rb
+++ db/migrate/20250422105615_add_columns_to_race_event.rb
@@ -3,5 +3,5 @@ class AddColumnsToRaceEvent < ActiveRecord::Migration[7.2]
     add_reference :race_events, :race, null: false, foreign_key: true
     add_reference :race_events, :event, null: false, foreign_key: true
   end
-  add_index :race_events,[:race_id, :events_id], unique: true
+    add_index :race_events,[:race_id, :events_id], unique: true
 end
