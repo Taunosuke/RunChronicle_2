f01d863f3167667aa28ca53cb5cf501d8d3d0ff2
Hamamoto Manabu <hamamotomanabu@MacBook-Air-2.local>
2025-05-31 20:46:44 +0900

    WIP: 一時保存のコミット

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   resources :races do
     resource :race_result, only: [ :new, :create, :edit, :update, :show ]
   end
+  resources :race_result do
+    resources :race_result_comment, only: [ :create, :destroy]
+  end
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
