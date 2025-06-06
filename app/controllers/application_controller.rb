1aeed698a9b9c4b98eb63f6472c1af00519d3716
Hamamoto Manabu <hamamotomanabu@MacBook-Air-2.local>
2025-06-06 19:35:53 +0900

    Lint修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     added_attrs = [ :name, :email, :password, :password_confirmation ]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:password])
   end
 end
