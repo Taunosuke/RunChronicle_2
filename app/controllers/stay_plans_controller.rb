4ceb6579344d55d89b5cea452d2475318bb5b477
Hamamoto Manabu <hamamotomanabu@MacBook-Air-2.local>
2025-06-12 20:50:02 +0900

    フォームの作成

--- app/controllers/stay_plans_controller.rb
+++ app/controllers/stay_plans_controller.rb
@@ -6,11 +6,17 @@ class StayPlansController < ApplicationController
   def create
    @stay_plan = StayPlan.new(stay_plan_params)
    @stay_plan.save
+   redirect_to race_path(set_race)
   end
 end
+
 
 private
 
 def stay_plan_params
   params.require(:stay_plan).permit(:place_name, :address, :check_in_time, :check_out_time, :note)
+end
+
+def set_race
+    @race = Race.find(params[:race_id])
 end
