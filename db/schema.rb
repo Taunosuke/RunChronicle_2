f0b74f22b408d17723b70d33dbe33e0e1add341b
Hamamoto Manabu <hamamotomanabu@MacBook-Air-2.local>
2025-07-01 20:01:33 +0900

    Itemテーブルのマイグレート

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_09_113325) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_01_105935) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,12 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_09_113325) do
   create_table "events", force: :cascade do |t|
     t.string "event"
     t.float "distance"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
