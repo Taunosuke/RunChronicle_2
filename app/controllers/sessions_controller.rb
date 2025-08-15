5affd260d267d30358ce8a63df060523cb70d59d
Taunosuke <hmskm2012w@gmail.com>
2025-08-15 11:33:14 +0900

    Complete Google OAuth authentication implementation - WORKING q q

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,21 @@
 class SessionsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:create]
+  skip_before_action :authenticate_user!, only: [ :create ]
 
   def create
-    auth = request.env['omniauth.auth']
+    auth = request.env[ 'omniauth.auth' ]
     user = find_or_create_from_auth_hash(auth)
 
     if user
       sign_in(user, bypass: true)
-      redirect_to root_path, notice: 'ログインしました'
+      redirect_to root_path, notice: "ログインしました"
     else
-      redirect_to new_user_registration_path, alert: 'ログインに失敗しました'
+      redirect_to new_user_registration_path, alert: "ログインに失敗しました"
     end
   end
 
   def destroy
     sign_out(current_user)
-    redirect_to root_path, notice: 'ログアウトしました'
+    redirect_to root_path, notice: "ログアウトしました"
   end
 
   private
@@ -23,7 +23,7 @@ class SessionsController < ApplicationController
   def find_or_create_from_auth_hash(auth)
     email = auth.info.email
     name = auth.info.name
-    
+
     User.find_or_create_by(email: email) do |user|
       user.name = name
       user.password = SecureRandom.hex(10)
