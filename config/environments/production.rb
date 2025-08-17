0882323d80bc055e513bd333da882e7564835d6c
Taunosuke <hmskm2012w@gmail.com>
2025-08-17 16:26:24 +0900

    独自ドメインの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.active_record.attributes_for_inspect = [ :id ]
 
-  config.hosts << "runchronicle.jp"      # 独自ドメイン 
+  config.hosts << "runchronicle.jp"      # 独自ドメイン
   config.hosts << "www.runchronicle-2.onrender.com"  # サブドメイン
 end
