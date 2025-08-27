5109d602c80f26158f70b8735c4026c9238c8e14
Taunosuke <hmskm2012w@gmail.com>
2025-08-27 19:53:44 +0900

    持ち物管理のバグ修正およびレイアウト調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [ :edit, :update, :destroy]
   def index
     @items = current_user.items
+    @item = Item.new
   end
 
   def new
@@ -10,31 +12,32 @@ class ItemsController < ApplicationController
   def create
     @item = current_user.items.build(item_params)
     if @item.save
-      redirect_to items_path, notice: "Item was successfully created."
+      redirect_to items_path, notice: "アイテムが作成されました"
     else
-      render :new
+      @items = current_user.items  # ← エラー時に一覧も表示するため
+      render :index  # ← indexテンプレートを再表示
     end
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to items_path, notice: "Item was successfully deleted."
-  end
-
-  def edit
-    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to items_path, notice: "アイテムが削除されました"
   end
 
   def update
-    if @item = Item.update(item_params)
-      redirect_to @item
+    if @item.update(item_params)
+      redirect_to items_path, notice: "アイテムが更新されました"
     else
-      render
+      render :edit
     end
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def item_params
     params.require(:item).permit(:name)
   end
